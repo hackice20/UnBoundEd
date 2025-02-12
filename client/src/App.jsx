c9af5702258bc6635689d830184ab8ccbfeff68e
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2025-02-12 22:44:26 +0530

    Configured React, Tailwind and ShadCN

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return <div>
+    <h1>Hello, Vite!</h1>
+    <Button>Click me</Button>
+  </div>;
+};
+
+export default App;
