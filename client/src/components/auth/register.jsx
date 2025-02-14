1d5c3f7495cfdf3058a15bb19d099638d2d786e3
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2025-02-14 23:38:33 +0530

    Add registration component with form and input fields

---
+++ client/src/components/auth/register.jsx
@@ -0,0 +1,88 @@
+import { GraduationCap } from "lucide-react";
+import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+const Register = ({ setTab }) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col items-center justify-center">
+        <GraduationCap className="w-16 h-16" />
+        <h1 className="text-3xl font-bold">Welcome Back</h1>
+        <p className="text-center text-gray-600">
+          Join Now – Empower Your Learning Journey!
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <form className="flex flex-col gap-3 mt-5">
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">Name</label>
+            <Input
+              type="name"
+              className="w-full"
+              placeholder="John Doe"
+              name="name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              User Name
+            </label>
+            <Input
+              type="userName"
+              className="w-full"
+              placeholder="johnDoe123"
+              name="userName"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              Email Address
+            </label>
+            <Input
+              type="email"
+              className="w-full"
+              placeholder="you@example.com"
+              name="email"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              Password
+            </label>
+            <Input
+              type="password"
+              className="w-full"
+              placeholder="••••••••"
+              name="password"
+            />
+          </div>
+
+          <div className="w-full mt-5">
+            <Button
+              className="w-full text-sm tracking-wider uppercase px-12 py-6 bg-primary hover:bg-purple-700 bg-purple-600"
+              type="submit"
+            >
+              Sign In
+            </Button>
+          </div>
+        </form>
+        <div className="mt-1">
+          <p className="text-center">
+            Already have an account?{" "}
+            <span
+              className="text-purple-600 font-semibold cursor-pointer"
+              onClick={() => {
+                setTab("login");
+              }}
+            >
+              Login Now
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
